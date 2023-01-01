bc175522c80f049ca079af99c5ba59150060640a
Max <zeetreex@gmail.com>
2023-01-01 20:47:38 +0200

    tested first endpoint in swagger, changed IActionResult to simple ActionResult for exact response type

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -11,5 +12,21 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var superHeroes = new List<SuperHero>
+            {
+                new SuperHero
+                    { Id = 1, 
+                        Name = "Spider Man", 
+                        FirstName = "Peter", 
+                        LastName = "Parker", 
+                        Place = "New York City" 
+                    }
+            };
+
+            return Ok(superHeroes);
+        }
     }
 }
