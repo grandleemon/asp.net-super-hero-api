ac3cd26d4eb77a6b843ae85a0d169735b65e7ec9
Max <zeetreex@gmail.com>
2023-01-01 22:54:53 +0200

    installed entity framework, connected to a database, first migration

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using WebApplication1.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
 
