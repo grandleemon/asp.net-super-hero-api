4a081e072bc7b787164931f0276c91788474c401
Max <zeetreex@gmail.com>
2023-01-01 20:57:54 +0200

    endpoint to get a single hero by an id

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -12,21 +12,39 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private static List<SuperHero> superHeroes = new List<SuperHero>
+        {
+            new SuperHero
+            {
+                Id = 1,
+                Name = "Spider Man",
+                FirstName = "Peter",
+                LastName = "Parker",
+                Place = "New York City"
+            },
+            new SuperHero
+            {
+                Id = 2,
+                Name = "Iron Man",
+                FirstName = "Tony",
+                LastName = "Stark",
+                Place = "Malibu"
+            }
+        };
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
         {
-            var superHeroes = new List<SuperHero>
-            {
-                new SuperHero
-                    { Id = 1, 
-                        Name = "Spider Man", 
-                        FirstName = "Peter", 
-                        LastName = "Parker", 
-                        Place = "New York City" 
-                    }
-            };
-
             return Ok(superHeroes);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetSingleHero(int id)
+        {
+            var hero = superHeroes.Find(sh => sh.Id == id);
+
+            if (hero is null) return NotFound("Sorry, but this hero does not exist");
+            
+            return Ok(hero);
         }
     }
 }
